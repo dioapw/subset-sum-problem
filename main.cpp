2110eb80d107bb728b3644331d1772e3a64fabd0
Dio Aditya Putra Wardhono <55073908+dioapw@users.noreply.github.com>
2021-03-07 22:27:19 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,47 +1,44 @@
 // code inspired by
 // https://www.tutorialspoint.com/Subset-Sum-Problem
 // https://iq.opengenus.org/subset-sum-problem-backtracking/
-
 #include <iostream>
 using namespace std;
 
-void printSubset (int subSet[], int panjang)
+void printSubset(int subSet[], int panjang)
 {
+	for (int i = 0; i < panjang - 1; i++)
+	{
+		cout << subSet[i] << ", ";
 
-  for (int i = 0; i < panjang - 1; i++)
-    {
+	}
 
-      cout << subSet[i] << ", ";
-
-    }
-
-  cout << subSet[panjang - 1] << "\n";
+	cout << subSet[panjang - 1] << "\n";
 
 }
 
-void backTrack (int kotak[], int panjangKotak, int subSet[], int panjangSubSet, int jumlahSementara, int jumlahDicari, int nilaiAwal)
+void backTrack(int kotak[], int panjangKotak, int subSet[], int panjangSubSet, int jumlahSementara, int jumlahDicari, int nilaiAwal)
 {
-  if (jumlahSementara == jumlahDicari)
-    {
-        printSubset (kotak, panjangKotak); //print subsetnya
-        backTrack (kotak, panjangKotak - 1, subSet, panjangSubSet,jumlahSementara - subSet[nilaiAwal - 1], jumlahDicari,nilaiAwal);   //untuk subset lainnya
-    }
-  else
-    {
-        for (int i = nilaiAwal; i < panjangSubSet; i++) //temukan simpul sepanjang lebarnya
-	    {
-	        kotak[panjangKotak] = subSet[i];
-	        backTrack (kotak, panjangKotak + 1, subSet, panjangSubSet, jumlahSementara + subSet[i], jumlahDicari, i + 1); //lakukan untuk node selanjutnya secara mendalam
+	if (jumlahSementara == jumlahDicari)
+	{
+		printSubset(kotak, panjangKotak);	//print subsetnya
+		backTrack(kotak, panjangKotak - 1, subSet, panjangSubSet, jumlahSementara - subSet[nilaiAwal - 1], jumlahDicari, nilaiAwal);	//untuk subset lainnya
+	}
+	else
+	{
+		for (int i = nilaiAwal; i < panjangSubSet; i++)	//temukan simpul sepanjang lebarnya
+		{
+			kotak[panjangKotak] = subSet[i];
+			backTrack(kotak, panjangKotak + 1, subSet, panjangSubSet, jumlahSementara + subSet[i], jumlahDicari, i + 1);	//lakukan untuk node selanjutnya secara mendalam
 
-	    }
-    }
+		}
+	}
 }
 
-int main ()
+int main()
 {
-  int set[] = { 3, 8, 4, 6, 1, 2 };
-  int panjangSet = sizeof (set) / sizeof (set[0]);
-  int *kotak = new int[panjangSet];  //membuat subset array untuk penyimpanan sementara sehingga subset dapat di print
-  backTrack (kotak, 0, set, panjangSet, 0, 12, 0);
-  delete[]kotak;
+	int set[] = { 3, 8, 4, 6, 1, 2 };
+	int panjangSet = sizeof(set) / sizeof(set[0]);
+	int *kotak = new int[panjangSet];	//membuat subset array untuk penyimpanan sementara sehingga subset dapat di print
+	backTrack(kotak, 0, set, panjangSet, 0, 12, 0);
+	delete[] kotak;
 }
