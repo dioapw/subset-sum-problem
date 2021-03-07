189d5e9f5579745b19b9c833c88fb63e9b07fb84
Dio Aditya Putra Wardhono <55073908+dioapw@users.noreply.github.com>
2021-03-07 22:10:55 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
+// code inspired by
+// https://www.tutorialspoint.com/Subset-Sum-Problem
+// https://iq.opengenus.org/subset-sum-problem-backtracking/
+
 #include <iostream>
 using namespace std;
 
-void
-printSubset (int subSet[], int panjang)
+void printSubset (int subSet[], int panjang)
 {
 
   for (int i = 0; i < panjang - 1; i++)
@@ -20,15 +23,15 @@ void backTrack (int kotak[], int panjangKotak, int subSet[], int panjangSubSet, 
 {
   if (jumlahSementara == jumlahDicari)
     {
-        printSubset (kotak, panjangKotak);
-        backTrack (kotak, panjangKotak - 1, subSet, panjangSubSet,jumlahSementara - subSet[nilaiAwal - 1], jumlahDicari,nilaiAwal);
+        printSubset (kotak, panjangKotak); //print subsetnya
+        backTrack (kotak, panjangKotak - 1, subSet, panjangSubSet,jumlahSementara - subSet[nilaiAwal - 1], jumlahDicari,nilaiAwal);   //untuk subset lainnya
     }
   else
     {
-        for (int i = nilaiAwal; i < panjangSubSet; i++)
+        for (int i = nilaiAwal; i < panjangSubSet; i++) //temukan simpul sepanjang lebarnya
 	    {
 	        kotak[panjangKotak] = subSet[i];
-	        backTrack (kotak, panjangKotak + 1, subSet, panjangSubSet, jumlahSementara + subSet[i], jumlahDicari, i + 1);
+	        backTrack (kotak, panjangKotak + 1, subSet, panjangSubSet, jumlahSementara + subSet[i], jumlahDicari, i + 1); //lakukan untuk node selanjutnya secara mendalam
 
 	    }
     }
@@ -38,7 +41,7 @@ int main ()
 {
   int set[] = { 3, 8, 4, 6, 1, 2 };
   int panjangSet = sizeof (set) / sizeof (set[0]);
-  int *kotak = new int[panjangSet];
+  int *kotak = new int[panjangSet];  //membuat subset array untuk penyimpanan sementara sehingga subset dapat di print
   backTrack (kotak, 0, set, panjangSet, 0, 12, 0);
   delete[]kotak;
 }
